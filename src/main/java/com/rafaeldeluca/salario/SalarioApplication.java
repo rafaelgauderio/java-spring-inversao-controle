1644cafe777e3078ea820b762f3f0f127090a9a4
Rafael de Luca <deluca1712@gmail.com>
2023-02-15 14:55:15 -0300

    Implementando Classe CommandLineRunner

--- src/main/java/com/rafaeldeluca/salario/SalarioApplication.java
+++ src/main/java/com/rafaeldeluca/salario/SalarioApplication.java
@@ -1,14 +1,19 @@
 package com.rafaeldeluca.salario;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SalarioApplication {
+public class SalarioApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SalarioApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Salário Líquido");
+	}
 }
